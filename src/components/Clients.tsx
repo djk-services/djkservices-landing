7cea33c18850f147a040aaa27c76394383f9d3ca
David Smith <dmsmith@Davids-MacBook-Air.local>
2025-04-18 15:10:35 -0400

    missed the second marquee

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -50,7 +50,7 @@ export const Clients = () => {
                   <img 
                     src={client.logo} 
                     alt={client.name} 
-                    className="max-w-full max-h-full object-contain" 
+                    className="max-w-full max-h-full object-contain dark:invert dark:hue-rotate-180 dark:contrast-90 dark:brightness-100" 
                   />
                 </div>
               ))}
